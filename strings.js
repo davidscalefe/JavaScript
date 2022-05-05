6b7e7f56befddfb9ac9c7c235c4015bb3dedce51
daviscalefe <72842485+davidscalefe@users.noreply.github.com>
2022-05-05 20:56:14 -0300

    Primeiros passos com JavaScript

---
+++ strings.js
@@ -0,0 +1,28 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log('Escola ' + escola + "!")
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana, Maria, Pedro'.split(','))
+
+/* Template String */
+
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `Olá ${nome}`
+console.log(concatenacao, template)
+
+// expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
---
+++ variaveisEConstantes.js
@@ -0,0 +1,15 @@
+var a = 5
+let b = 3
+
+console.log(a, b)
+
+var a = 50
+b = 30                      // variavel let
+
+console.log(a, b)
+
+const c = 4
+
+console.log(c)
+
+// c = 5                    'const não pode ser alterado é uma variavel reservada'
